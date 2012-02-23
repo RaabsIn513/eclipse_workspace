5809e31dbe073519625b1b40f18b56e9c0645a14
RaabsIn513 <hildebrandtr12@gmail.com>
2012-02-23 14:48:44 -0500

    updates on GUIs and operational mode activity started

---
+++ SAS_revA/src/senior/project/Operational_Mode_Activity.java
@@ -0,0 +1,13 @@
+package senior.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Operational_Mode_Activity extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.);
+	}
+}
